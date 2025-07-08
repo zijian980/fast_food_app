eb3c01ee923ee845abc9abb70e6cad352fcd3636
Zijian <163723805+zijian980@users.noreply.github.com>
2025-07-09 00:38:28 +0800

    Integrate custom fonts, images, and icons; configure Tailwind with extended theme settings and Nativewind compatibility.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
-import { Stack } from "expo-router";
+import {SplashScreen, Stack} from "expo-router";
 import "./globals.css";
+import { useEffect} from "react";
+import { useFonts } from "expo-font";
 
 export default function RootLayout() {
-  return <Stack />;
+  const [fontsLoaded, error] = useFonts({
+    "Quicksand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
+    "Quicksand-Regular": require("../assets/fonts/Quicksand-Regular.ttf"),
+    "Quicksand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
+    "Quicksand-SemiBold": require("../assets/fonts/Quicksand-SemiBold.ttf"),
+    "Quicksand-Light": require("../assets/fonts/Quicksand-Light.ttf"),
+  })
+
+  useEffect(()=>{
+    if(error) throw error;
+    if(fontsLoaded) SplashScreen.hideAsync();
+  },[fontsLoaded, error])
+
+  return <Stack screenOptions={{headerShown: false}}/>;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function Index() {
     return (
         <View className="flex-1 items-center justify-center bg-white">
-            <Text className="text-xl font-bold text-blue-500">
-                Welcome to Nativewind!
+            <Text className="text-xl text-center font-bold text-primary font-quicksand">
+                Welcome to my react native app!
             </Text>
         </View>
     );
