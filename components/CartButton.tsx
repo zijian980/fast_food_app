9664538b1b5c9c45a3930c2b4b13375569446536
Zijian <163723805+zijian980@users.noreply.github.com>
2025-07-22 23:06:19 +0800

    Add cart functionality with Zustand store, `CartItem` component, and cart screen enhancements.

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -1,12 +1,15 @@
 import {View, Text, TouchableOpacity, Image} from 'react-native'
 import React from 'react'
 import {images} from "@/constants";
+import {useCartStore} from "@/store/cart.store";
+import {router} from "expo-router";
 
 const CartButton = () => {
-    const totalItems=10;
+    const {getTotalItems} = useCartStore();
+    const totalItems = getTotalItems();
 
     return (
-        <TouchableOpacity className="cart-btn" onPress={()=>{}}>
+        <TouchableOpacity className="cart-btn" onPress={()=> router.push('/cart')}>
             <Image source={images.bag} className="size-5" resizeMode="contain"/>
             {totalItems > 0 && (
                 <View className="cart-badge">
